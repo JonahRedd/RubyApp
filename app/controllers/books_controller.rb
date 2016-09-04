a604e121e9d8143fe387ded0cb5cf109d1e17d08
Jonah Redd <youness.echchadi@gmail.com>
2016-09-04 20:43:28 +0100

    styling completed

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
     before_action :find_book, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_user!, only: [:new, :edit]
     def index
         if params[:category].blank?
             @books = Book.all.order("created_at DESC")
@@ -10,6 +11,11 @@ class BooksController < ApplicationController
     end
 
     def show
+        if @book.reviews.blank?
+            @average_review = 0
+        else 
+            @average_review = @book.reviews.average(:rating).round(2)
+        end
     end
 
     def edit
